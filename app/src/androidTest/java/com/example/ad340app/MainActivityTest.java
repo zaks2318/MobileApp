8afc5112966b508231b25b089f8507cfad02afdb
zaks2318 <68169592+zaks2318@users.noreply.github.com>
2021-04-13 16:32:21 -0700

    fix test file

--- app/src/androidTest/java/com/example/ad340app/MainActivityTest.java
+++ app/src/androidTest/java/com/example/ad340app/MainActivityTest.java
@@ -1,11 +1,17 @@
 package com.example.ad340app;
 
+import androidx.annotation.ContentView;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -13,4 +19,10 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void hasTextOnScreen(){
+        onView(withId(R.id.helloWorldText))
+                .check(matches(withText(R.string.Hello_frament)));
+    }
 }
